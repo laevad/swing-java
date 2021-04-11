66456b34201ecc5bba57fcd72231c5a20c6ff460
laevad <aldave.tabian@ustp.edu.ph>
2021-04-12 02:25:58 +0800

    Password field

--- src/gui/PrefsDialog.java
+++ src/gui/PrefsDialog.java
@@ -10,11 +10,18 @@ public class PrefsDialog extends JDialog {
     private JButton cancelButton;
     private  JSpinner portSpinner;
     private SpinnerNumberModel spinnerNumberModel;
+    private JTextField userfield;
+    private JPasswordField passwordField;
     public PrefsDialog(JFrame parentFrame){
         super(parentFrame, "Preferences", false);
 
         okButton = new JButton("OK");
         cancelButton = new JButton("Cancel");
+        userfield = new JTextField(10);
+        passwordField = new JPasswordField(10);
+
+        passwordField.setEchoChar('*');
+
 
         spinnerNumberModel = new SpinnerNumberModel(3306, 0, 9999, 1);
         portSpinner = new JSpinner(spinnerNumberModel);
@@ -24,6 +31,34 @@ public class PrefsDialog extends JDialog {
         GridBagConstraints gridBagConstraints = new GridBagConstraints();
 
         gridBagConstraints.gridy = 0;
+
+        // row
+        gridBagConstraints.weightx = 1;
+        gridBagConstraints.weighty = 1;
+        gridBagConstraints.fill = GridBagConstraints.NONE;
+
+        gridBagConstraints.gridx = 0;
+
+        add(new JLabel("User: "), gridBagConstraints);
+
+        gridBagConstraints.gridx++;
+        add(userfield, gridBagConstraints);
+
+        // row
+        gridBagConstraints.gridy++;
+
+        gridBagConstraints.weightx = 1;
+        gridBagConstraints.weighty = 0.001;
+        gridBagConstraints.fill = GridBagConstraints.NONE;
+
+        gridBagConstraints.gridx = 0;
+
+        add(new JLabel("Password: "), gridBagConstraints);
+
+        gridBagConstraints.gridx++;
+        add(passwordField, gridBagConstraints);
+        // row
+        gridBagConstraints.gridy++;
         gridBagConstraints.weightx = 1;
         gridBagConstraints.weighty = 1;
         gridBagConstraints.fill = GridBagConstraints.NONE;
@@ -49,7 +84,10 @@ public class PrefsDialog extends JDialog {
             @Override
             public void actionPerformed(ActionEvent e) {
                 int value = (int) portSpinner.getValue();
-                System.out.println(value);
+
+                String user = userfield.getText();
+                char[] password =  passwordField.getPassword();
+                System.out.println(user + " : "+ new String(password));
                 setVisible(false);
             }
         });
@@ -61,7 +99,7 @@ public class PrefsDialog extends JDialog {
             }
         });
 
-        setSize(250, 150);
+        setSize(250, 200);
         setResizable(false);
         setLocationRelativeTo(parentFrame);
     }
