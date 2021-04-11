2352d5d2099d75ec3dfa0160aecc68f87771d586
laevad <aldave.tabian@ustp.edu.ph>
2021-04-11 09:27:29 +0800

    adding components layouts buttons and text areas

--- src/App.java
+++ src/App.java
@@ -6,10 +6,7 @@ public class App {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                JFrame frame = new JFrame("Hello World"); // title bar
-                frame.setVisible(true); // make it visible
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //to terminate
-                frame.setSize(600,500); // to set size
+                new MainFrame();
             }
         });
 
---
+++ src/MainFrame.java
@@ -0,0 +1,37 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrame extends JFrame {
+    // component - private instance variables
+    private JTextArea textArea;
+    private JButton button;
+    public MainFrame(){
+        super("Hello World"); // title bar
+        setVisible(true); // make it visible
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //to terminate
+        setSize(600,500); // to set size
+
+        // Border Layout
+        /*
+          Every content pane is initialized to use a BorderLayout.
+          (As Using Top-Level Containers explains,
+          the content pane is the main container in all
+          frames, applets, and dialogs.)
+          A BorderLayout places components in up to five areas:
+          top, bottom, left, right, and center.
+          All extra space is placed in the center area.
+          Tool bars that are created using
+          JToolBar must be created within a
+          BorderLayout container,
+          https://docs.oracle.com/javase/tutorial/uiswing/layout/visual.html#border
+         */
+        setLayout(new BorderLayout());
+
+        // create new object
+        textArea = new JTextArea();
+        button = new JButton("Click Me");
+
+        add(textArea, BorderLayout.CENTER);
+        add(button, BorderLayout.SOUTH);
+    }
+}
