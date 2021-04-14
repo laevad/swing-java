179acdf2d3de9785d4bfccf605e7d2ce6c107072
laevad <aldave.tabian@ustp.edu.ph>
2021-04-14 15:26:10 +0800

    setting cursor

--- src/gui/ProgressDialog.java
+++ src/gui/ProgressDialog.java
@@ -78,6 +78,11 @@ public class ProgressDialog extends JDialog {
                 }else {
                     progressBar.setValue(0);
                 }
+                if (visible){
+                    setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
+                }else {
+                    setCursor(Cursor.getDefaultCursor());
+                }
                 ProgressDialog.super.setVisible(visible);
             }
         });
