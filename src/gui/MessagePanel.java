b32b14e76a4ef58ddd30928deb8a10ea258deede
laevad <aldave.tabian@ustp.edu.ph>
2021-04-14 12:54:27 +0800

    multi threading in swing the SwingWorker class

--- src/gui/MessagePanel.java
+++ src/gui/MessagePanel.java
@@ -13,8 +13,11 @@ import javax.swing.tree.DefaultTreeCellRenderer;
 import javax.swing.tree.TreeCellEditor;
 import javax.swing.tree.TreeSelectionModel;
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class MessagePanel extends JPanel {
     private JTree serverTree;
@@ -53,10 +56,46 @@ public class MessagePanel extends JPanel {
                     selectedServers.remove(serverId);
                 }
                 messageServer.setSelectedServers(selectedServers);
+                retrieveMessages();
+               
+            }
+
+            private void retrieveMessages() {
+                SwingWorker<List<Message>, Integer> worker = new SwingWorker<List<Message>, Integer>(){
+                    @Override
+                    protected List<Message> doInBackground() throws Exception {
+                        List<Message> retrieveMessages = new ArrayList<Message>();
+                        int count = 0;
+                        for (Message message: messageServer){
+                            System.out.println(message.getTitle());
+                            retrieveMessages.add(message);
+                            count++;
+                            publish(count);
+                        }
+                        return retrieveMessages;
+                    }
+
+                    @Override
+                    protected void process(List<Integer> counts) {
+                        int retrieved = counts.get(counts.size() - 1);
+                        System.out.println("Got "+ retrieved +" messages.");
+                    }
+
+                    @Override
+                    protected void done() {
+                        try {
+                            List<Message> retrieveMessages = get();
+                            System.out.println("Retrieve "+ retrieveMessages.size()+" messages.");
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        } catch (ExecutionException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                worker.execute();
                 System.out.println("Messages waiting: "+messageServer.getMessageCount());
-                for (Message message: messageServer){
-                    System.out.println(message.getTitle());
-                }
+
             }
 
             @Override
